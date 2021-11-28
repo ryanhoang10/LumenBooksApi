54ff0ab198461d0446d9b2d59f3c5bd582992fe9
ryanhoang10 <55223126+ryanhoang10@users.noreply.github.com>
2021-11-28 01:18:14 -0600

    create functions and standardize response

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -8,14 +8,17 @@ use App\Models\Book;
 
 class BookController extends Controller
 {
+    use ApiResponser; 
+    
     /**
-     * Create a new controller instance.
-     *
-     * @return void
+     * Return the list of authors
+     * @return Illuminate\Http\Response
      */
-    public function __construct()
+    public function index() 
     {
-        //
+        $books = Book::all();
+
+        return $this->successResponse($books);
     }
 
     /**
@@ -24,7 +27,18 @@ class BookController extends Controller
      */
     public function store(Request $request) 
     {
+        $rules = [
+            'title'       => 'required|max:255',
+            'description' => 'required|max:255',
+            'price'       => 'required|min:1',
+            'author_id'   => 'required|min:1',
+        ];
 
+        $this->validate($request, $rules);
+
+        $book = Book::create($request->all());
+
+        return $this->successResponse($book, Response::HTTP_CREATED);
     }
 
     /**
@@ -33,7 +47,9 @@ class BookController extends Controller
      */
     public function show($book) 
     {
-  
+        $book = Book::findOrFail($book);
+
+        return $this->successResponse($book);
     }
 
     /**
@@ -42,7 +58,26 @@ class BookController extends Controller
      */
     public function update(Request $request, $book) 
     {
+        $rules = [
+            'title'       => 'required|max:255',
+            'description' => 'required|max:255',
+            'price'       => 'required|min:1',
+            'author_id'   => 'required|min:1',
+        ];
 
+        $this->validate($request, $rules);
+
+        $book = Book::findOrFail($book);
+
+        $book->fill($request->all());
+
+        if($book->isClean()) {
+            return $this->errorResponse('At least one value must change', Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        $book->save();
+
+        return $this->successResponse($book);
     }
 
     /**
@@ -51,6 +86,10 @@ class BookController extends Controller
      */
     public function destroy($book) 
     {
-      
+      $book = Book::findOrFail($book);
+
+      $book->delete();
+
+      return $this->successResponse($book);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@
 
 $router->get('/books', 'BookController@index');
 $router->post('/books', 'BookController@store');
-$router->get('/books/{author}', 'BookController@show');
-$router->put('/books/{author}', 'BookController@update');
-$router->patch('/books/{author}', 'BookController@update');
-$router->delete('/books/{author}', 'BookController@destroy');
+$router->get('/books/{book}', 'BookController@show');
+$router->put('/books/{book}', 'BookController@update');
+$router->patch('/books/{book}', 'BookController@update');
+$router->delete('/books/{book}', 'BookController@destroy');
